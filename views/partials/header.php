cc05ad6b295c400808eb2e46c3e101ed34238eb8
kalczugag <kalczugag@gmail.com>
2024-09-16 16:01:31 +0200

    first commit

---
+++ views/partials/header.php
@@ -0,0 +1,4 @@
+<header class="fixed top-0 left-0 right-0 flex flex-row items-center justify-between p-2 px-6 bg-[#1e1f20] text-white shadow">
+    <h1 class="text-2xl">CRM</h1>
+    <a href="/login">Login</a>
+</header>
